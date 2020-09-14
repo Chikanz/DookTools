0c5166d7abf5ca7f251accf60ac86cb26af75129
Zac Lucarelli <Zac.Lucarelli@outlook.com>
2020-09-14 11:45:09 +1000

    Initial commit :tada:

---
+++ Scripts/Animatey.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dook.tools.animatey
+{
+    [Serializable]
+    public class Animatey
+    {
+        public AnimationCurve Curve;
+        public float duration;
+        public Vector2 FromTo = new Vector2(0, 1);
+        private float chainTime = -1;
+
+        public delegate void OnAnimate(float val, object args = null);
+
+        public OnAnimate Action;
+
+        private bool chainFired;
+        public EventHandler Chain;
+        public EventHandler Done;
+
+        public bool playedInReverse { get; private set; }
+
+        public IEnumerator Routine(bool reverse = false, float speed = 1, object args = null)
+        {
+            playedInReverse = reverse;
+
+            float timeElapsed = 0;
+            while (timeElapsed <= duration)
+            {
+                var i = reverse ? Curve.Evaluate(1 - (timeElapsed / duration)) : Curve.Evaluate(timeElapsed / duration);
+
+                i = Mathf.Clamp01(i);
+                var val = Mathf.Lerp(FromTo.x, FromTo.y, i);
+
+                Action?.Invoke(val, args);
+
+                if (chainTime > 0 && i > chainTime && !chainFired)
+                {
+                    Chain?.Invoke(this, null);
+                    chainFired = true;
+                }
+
+                timeElapsed += Time.deltaTime * speed;
+
+                yield return null;
+            }
+
+            Done?.Invoke(this, null);
+            chainFired = false;
+        }
+
+        public Animatey ChainAt(float duration)
+        {
+            chainTime = duration;
+            return this;
+        }
+
+        public Animatey SetGoal(float goal)
+        {
+            FromTo.y = goal;
+            return this;
+        }
+
+        public void Play(MonoBehaviour behaviour, bool reverse = false, float speed = 1)
+        {
+            behaviour.StopCoroutine(Routine());
+            behaviour.StartCoroutine(Routine(reverse, speed));
+        }
+    }
+}
---
+++ Scripts/DookTools.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace dook.tools
+{
+
+
+    public static class DookTools
+    {
+        /// <summary>
+        /// Clamp a vector's length
+        /// </summary>
+        public static Vector3 ClampMagnitude(Vector3 v, float min, float max)
+        {
+            double sm = v.sqrMagnitude;
+            if (sm > (double) max * (double) max) return v.normalized * max;
+            else if (sm < (double) min * (double) min) return v.normalized * min;
+            return v;
+        }
+
+        /// <summary>
+        /// Find length of an animator clip given a name
+        /// </summary>
+        /// <returns>Clip length, null if not found</returns>
+        public static float? GetACLength(RuntimeAnimatorController ac, string clipName)
+        {
+            return GetACLength(ac.animationClips, clipName);
+        }
+
+        /// <summary>
+        /// Find length of an animator clip given a name
+        /// </summary>
+        /// <returns>Clip length, null if not found</returns>
+        public static float? GetACLength(AnimationClip[] clips, string clipName)
+        {
+            foreach (var clip in clips)
+            {
+                if (clip.name.Contains(clipName)) //If it has the same name as your clip
+                {
+                    return clip.length;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Find length of an animator clip given a name
+        /// </summary>
+        /// <returns>Clip length, null if not found</returns>
+        public static float? GetACLength(Animator anim, string clipName)
+        {
+            return GetACLength(anim.runtimeAnimatorController, clipName);
+        }
+
+
+        /// <summary>
+        /// Fade an image from a color to another
+        /// </summary>
+        public static IEnumerator Fade(Image img, float duration, Color from, Color to)
+        {
+            float elapsed = 0;
+            float lerp = 0;
+            while (lerp < 1)
+            {
+                img.color = Color.Lerp(from, to, lerp);
+                lerp = elapsed / duration;
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        /// <summary>
+        /// Hijack a coroutine so that it can be nested synchrounously
+        /// https://jacksondunstan.com/articles/3241
+        /// </summary>
+        public static IEnumerator NestCoroutine(IEnumerator routine)
+        {
+            while (routine.MoveNext())
+            {
+                yield return routine.Current;
+            }
+        }
+
+        /// <summary>
+        /// Draw a ray using debug.draw
+        /// </summary>
+        public static void DrawRay(Ray r, Color c, float time)
+        {
+            Debug.DrawRay(r.origin, r.direction, c, time);
+        }
+
+        /// <summary>
+        /// Round a vector
+        /// </summary>
+        public static Vector3 Round(this Vector3 vector3, int decimalPlaces = 2)
+        {
+            float multiplier = 1;
+            for (int i = 0; i < decimalPlaces; i++)
+            {
+                multiplier *= 10f;
+            }
+
+            return new Vector3(
+                Mathf.Round(vector3.x * multiplier) / multiplier,
+                Mathf.Round(vector3.y * multiplier) / multiplier,
+                Mathf.Round(vector3.z * multiplier) / multiplier);
+        }
+
+        public static Vector3 ShittyPerlinVector(float x, float y)
+        {
+            return new Vector3(Mathf.PerlinNoise(x, y), Mathf.PerlinNoise(x * 2, y * 2),
+                Mathf.PerlinNoise(x * 3, y * 3));
+        }
+
+
+        /// <summary>
+        /// Sway a vector on X and Y (ie aiming a gun) 
+        /// </summary>
+        /// <param name="Accuracy"></param>
+        /// <returns>Swayed vector</returns>
+        public static Vector3 Sway(float Accuracy)
+        {
+            return new Vector3(
+                Accuracy * (Mathf.PerlinNoise(0, Time.time) * 2) - 1,
+                Accuracy * (Mathf.PerlinNoise(Time.time, 0) * 2) - 1,
+                0);
+        }
+
+        //https://answers.unity.com/questions/1007585/reading-and-setting-asn-objects-global-scale-with.html
+        /// <summary>
+        /// Sets a transform's scale globally
+        /// </summary>
+        public static void SetGlobalScale(this Transform transform, Vector3 globalScale)
+        {
+            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(globalScale.x / transform.lossyScale.x,
+                globalScale.y / transform.lossyScale.y, globalScale.z / transform.lossyScale.z);
+        }
+
+        /// <summary>
+        /// Wait for real seconds elapsed instead of using game time
+        /// </summary>
+        /// <param name="time">time to wait</param>
+        public static IEnumerator WaitForRealSeconds(float time)
+        {
+            float start = Time.realtimeSinceStartup;
+            while (Time.realtimeSinceStartup < start + time)
+            {
+                yield return null;
+            }
+        }
+
+        public static float Map(float value, float OldMin, float OldMax, float NewMin, float NewMax)
+        {
+            float oldRange = (OldMax - OldMin);
+            float newRange = (NewMax - NewMin);
+            float newValue = (((value - OldMin) * newRange) / oldRange) + NewMin;
+
+            return (newValue);
+        }
+
+        /// <summary>
+        /// Finds the transform closest to a point in an array 
+        /// </summary>
+        /// <returns>Closest transform</returns>
+        public static Transform FindClosest(this Component[] transforms, Vector3 pointToCompare)
+        {
+            Transform bestTarget = null;
+            float closestDistanceSqr = Mathf.Infinity;
+            Vector3 currentPosition = pointToCompare;
+            foreach (Component potentialTarget in transforms)
+            {
+                if (potentialTarget == null) continue;
+
+                Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+                float dSqrToTarget = directionToTarget.sqrMagnitude;
+                if (dSqrToTarget < closestDistanceSqr)
+                {
+                    closestDistanceSqr = dSqrToTarget;
+                    bestTarget = potentialTarget.transform;
+                }
+            }
+
+            return bestTarget;
+        }
+
+        /// <summary>
+        /// Returns the closest point on a line given a position 
+        /// </summary>
+        /// <param name="p">Current position</param>
+        /// <param name="a">Line Start</param>
+        /// <param name="b">Line End</param>
+        /// <returns></returns>
+        public static Vector3 GetClosestPointOnLine(Vector3 p, Vector3 a, Vector3 b)
+        {
+            return a + Vector3.Project(p - a, b - a);
+        }
+        
+
+        /// <summary>
+        /// Get point on bezier curve 
+        /// </summary>
+        /// <param name="p0">bezier start</param>
+        /// <param name="p1">bezier modifier</param>
+        /// <param name="p2">bezier end</param>
+        /// <param name="time">0-1 on how far along the curve to get</param>
+        /// <returns></returns>
+        public static Vector3 GetBezier(Vector3 p0, Vector3 p1, Vector3 p2, float time)
+        {
+            var t = InOutEase(time);
+            t = Mathf.Clamp01(t);
+            float oneMinusT = 1f - t;
+            return
+                oneMinusT * oneMinusT * p0 +
+                2f * oneMinusT * t * p1 +
+                t * t * p2;
+        }
+        public static float InOutEase(float k)
+        {
+            if ((k *= 2f) < 1f) return 0.5f * k * k;
+            return -0.5f * ((k -= 1f) * (k - 2f) - 1f);
+        }
+
+        public static float DTLerp(float a, float b, float factor)
+        {
+            return Mathf.Lerp(a, b, 1 - Mathf.Pow(factor, Time.deltaTime));
+        }
+
+        /// <summary>
+        /// Quick method to set color's alpha
+        /// </summary>
+        /// <param name="c"></param>
+        /// <param name="alpha"></param>
+        /// <returns></returns>
+        public static Color SetColorAlpha(Color c, float alpha)
+        {
+            Color newColor = c;
+            newColor.a = alpha;
+            return newColor;
+        }
+
+        /// <summary>
+        /// Draw a rect using debug.drawline
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="size"></param>
+        /// <param name="color"></param>
+        /// <param name="time">time rect is drawn for</param>
+        public static void DrawRect(Vector3 position, float size, Color color, float time)
+        {
+            Debug.DrawLine(position, new Vector3(position.x + size, position.y, position.z), color, time);
+            Debug.DrawLine(position, new Vector3(position.x, position.y - size, position.z), color, time);
+            Debug.DrawLine(new Vector3(position.x, position.y - size, position.z),
+                new Vector3(position.x + size, position.y - size, position.z), color, time);
+            Debug.DrawLine(new Vector3(position.x + size, position.y - size, position.z),
+                new Vector3(position.x + size, position.y, position.z), color, time);
+        }
+
+        /// <summary>
+        /// Round to decimal places
+        /// </summary>
+        /// <param name="places">How many decimal places to round to</param>
+        /// <returns></returns>
+        public static float RoundTo(this float a, float places)
+        {
+            return Mathf.Round(a / places) * places;
+        }
+
+        /// <summary>
+        /// Get the middle point of a bunch of points
+        /// </summary>
+        /// <param name="positions">Array of points to get average of</param>
+        /// <returns>Middle point of array</returns>
+        public static Vector3 GetMeanVector(Vector3[] positions)
+        {
+            if (positions.Length == 0)
+                return Vector3.zero;
+            float x = 0f;
+            float y = 0f;
+            float z = 0f;
+            foreach (Vector3 pos in positions)
+            {
+                x += pos.x;
+                y += pos.y;
+                z += pos.z;
+            }
+
+            return new Vector3(x / positions.Length, y / positions.Length, z / positions.Length);
+        }
+
+        //http://www.rorydriscoll.com/2016/03/07/frame-rate-independent-damping-using-lerp/
+        /// <summary>
+        /// Smooth lerp but takes delta time into account, use in lerp for interpolation
+        /// </summary>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static float DTFactor(float f) => 1 - Mathf.Pow(f, Time.deltaTime);
+
+        
+        #region Structs
+        public static KeyCode[] Numkeycodes =
+        {
+            KeyCode.Alpha1,
+            KeyCode.Alpha2,
+            KeyCode.Alpha3,
+            KeyCode.Alpha4,
+            KeyCode.Alpha5,
+            KeyCode.Alpha6,
+            KeyCode.Alpha7,
+            KeyCode.Alpha8,
+            KeyCode.Alpha9,
+            KeyCode.Alpha0,
+        };
+
+    }
+    
+    public struct TransformSnapShot
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+
+        public TransformSnapShot(Transform t)
+        {
+            Position = t.position;
+            Rotation = t.rotation;
+        }
+    }
+    #endregion
+}
+
